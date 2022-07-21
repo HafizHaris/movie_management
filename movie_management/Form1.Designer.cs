d6f743e3430034b5d86e5dd8f949062b4258fe64
Hafiz Haris Khan <haris.khan@techno-soft.com>
2022-07-21 18:04:56 +0500

    Add project files.

---
+++ movie_management/Form1.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace movie_management
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_name = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dt_release = new System.Windows.Forms.DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_writer = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_director = new System.Windows.Forms.TextBox();
+            this.grd_movies = new System.Windows.Forms.DataGridView();
+            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.director = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.writer = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.release_date = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_find = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grd_movies)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txt_name
+            // 
+            this.txt_name.Location = new System.Drawing.Point(23, 32);
+            this.txt_name.Name = "txt_name";
+            this.txt_name.Size = new System.Drawing.Size(142, 20);
+            this.txt_name.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(207, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(335, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "MMS - Movie Managment System";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.dt_release);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.txt_writer);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.txt_director);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.txt_name);
+            this.panel1.Location = new System.Drawing.Point(390, 75);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(381, 160);
+            this.panel1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(23, 122);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // dt_release
+            // 
+            this.dt_release.Location = new System.Drawing.Point(214, 82);
+            this.dt_release.Name = "dt_release";
+            this.dt_release.Size = new System.Drawing.Size(140, 20);
+            this.dt_release.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(213, 61);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Release Date";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 61);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Writer";
+            // 
+            // txt_writer
+            // 
+            this.txt_writer.Location = new System.Drawing.Point(21, 82);
+            this.txt_writer.Name = "txt_writer";
+            this.txt_writer.Size = new System.Drawing.Size(142, 20);
+            this.txt_writer.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(213, 11);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Director";
+            // 
+            // txt_director
+            // 
+            this.txt_director.Location = new System.Drawing.Point(214, 32);
+            this.txt_director.Name = "txt_director";
+            this.txt_director.Size = new System.Drawing.Size(142, 20);
+            this.txt_director.TabIndex = 3;
+            // 
+            // grd_movies
+            // 
+            this.grd_movies.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grd_movies.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.name,
+            this.director,
+            this.writer,
+            this.release_date});
+            this.grd_movies.Location = new System.Drawing.Point(140, 277);
+            this.grd_movies.Name = "grd_movies";
+            this.grd_movies.Size = new System.Drawing.Size(444, 150);
+            this.grd_movies.TabIndex = 3;
+            // 
+            // name
+            // 
+            this.name.HeaderText = "Name";
+            this.name.Name = "name";
+            // 
+            // director
+            // 
+            this.director.HeaderText = "Director";
+            this.director.Name = "director";
+            // 
+            // writer
+            // 
+            this.writer.HeaderText = "Writer";
+            this.writer.Name = "writer";
+            // 
+            // release_date
+            // 
+            this.release_date.HeaderText = "Release Date";
+            this.release_date.Name = "release_date";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(327, 253);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(82, 21);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "All Movies";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(151, 122);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Update";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(276, 122);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Remove";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name";
+            // 
+            // txt_find
+            // 
+            this.txt_find.Location = new System.Drawing.Point(21, 41);
+            this.txt_find.Name = "txt_find";
+            this.txt_find.Size = new System.Drawing.Size(151, 20);
+            this.txt_find.TabIndex = 0;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(19, 18);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(63, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Enter Name";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(21, 73);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "Find";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.button4);
+            this.panel2.Controls.Add(this.label7);
+            this.panel2.Controls.Add(this.txt_find);
+            this.panel2.Location = new System.Drawing.Point(24, 77);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(312, 158);
+            this.panel2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.grd_movies);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grd_movies)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_name;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_writer;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_director;
+        private System.Windows.Forms.DateTimePicker dt_release;
+        private System.Windows.Forms.DataGridView grd_movies;
+        private System.Windows.Forms.DataGridViewTextBoxColumn name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn director;
+        private System.Windows.Forms.DataGridViewTextBoxColumn writer;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn release_date;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_find;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ movie_management/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace movie_management
+{
+
+  
+    public partial class Form1 : Form
+    {
+        List<Movie> movies;
+        public Form1()
+        {
+            InitializeComponent();
+            movies = new List<Movie>();
+         
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void emptyTextBoxes()
+        {
+            txt_name.Text = string.Empty;
+            txt_director.Text = string.Empty;
+            txt_writer.Text = string.Empty;
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+        //add
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(txt_name.Text) || String.IsNullOrEmpty(txt_director.Text) || String.IsNullOrEmpty(txt_writer.Text))
+            {
+                MessageBox.Show("Please fill all the fields properly", "Warning",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else {
+
+
+                string release_date = dt_release.Value.ToShortDateString();
+
+                //store in a list
+                Movie m = new Movie();
+                m.name = txt_name.Text;
+                m.director = txt_director.Text;
+                m.writer = txt_writer.Text;
+                m.release_date = release_date;
+                movies.Add(m);
+
+
+                grd_movies.Rows.Add(txt_name.Text, txt_director.Text, txt_writer.Text, release_date);
+                emptyTextBoxes();
+            
+            }
+
+        }
+
+        class Movie
+        {
+            public string name { get; set; }
+
+            public string director { get; set; }
+
+            public string writer { get; set; }
+
+            public string release_date { get; set; }
+
+           
+        }
+        //find
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            string query = txt_find.Text;
+            var myItem = movies.Find(item => item.name == query);
+            if (myItem == null)
+            {
+                MessageBox.Show("Movie not found", "Warning",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                txt_name.Text = myItem.name;
+                txt_director.Text = myItem.director;
+                txt_writer.Text = myItem.writer;
+                dt_release.Value = Convert.ToDateTime(myItem.release_date);
+                txt_find.ReadOnly = true;
+                
+            }
+        }
+
+        //remove
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            var name = txt_find.Text;
+
+            if (String.IsNullOrEmpty(txt_find.Text))
+            {
+                MessageBox.Show("Please fill all the fields properly", "Warning",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                movies.RemoveAll(x => x.name == name);
+                txt_find.ReadOnly = false;
+                txt_find.Text = string.Empty;
+                emptyTextBoxes();
+                update_list();
+                MessageBox.Show("Movie deleted successfully", "Success message",
+                   MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            }
+        }
+
+        //update
+        private void button2_Click(object sender, EventArgs e)
+            {
+            var name = txt_find.Text;
+            var obj = movies.FirstOrDefault(x => x.name == name);
+
+            obj.name = txt_name.Text;
+            obj.director = txt_director.Text;
+            obj.writer = txt_writer.Text;
+            obj.release_date = dt_release.Value.ToShortDateString();
+            movies.Where(w => w.name == name).ToList().ForEach(s => s.name = txt_name.Text);
+            
+            update_list();
+            txt_find.Text = string.Empty;
+            txt_find.ReadOnly = false;
+            emptyTextBoxes();
+            
+
+        }
+
+        private void update_list() {
+            grd_movies.DataSource = movies;
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
